375dd849f7fba6f7b8fac233fd1f212c44f328b1
eduoo <44001810+eduoo@users.noreply.github.com>
2019-01-05 09:05:37 +0300

    Add project files.

---
+++ NUnitTest/MyMath.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTest
+{
+    public class MyMath
+    {
+        //Add method
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        //Subtraction method
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
